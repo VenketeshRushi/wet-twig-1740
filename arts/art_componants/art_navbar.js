2f91efc0cacd4abf90b7517f839a4150d68f8190
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-07-22 22:42:08 +0530

    day4

---
+++ arts/art_componants/art_navbar.js
@@ -0,0 +1,9 @@
+function nav(){
+    return `  
+    <p><a href="design.html">DESIGN</a></p>
+    <p><a href="art.html">ART</a></p>
+    <p><a href="collabration.html">COLLABORATIONS</a></p>
+`
+}
+
+export default nav;
