42283cd3ee50cfd799f304ecb95d1e3e0c79e12d
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-07-22 22:22:40 +0530

    Delete arts directory

--- arts/art_componants/art_navbar.js
+++
@@ -1,9 +0,0 @@
-function nav(){
-    return `  
-    <p><a href="design.html">DESIGN</a></p>
-    <p><a href="art.html">ART</a></p>
-    <p><a href="collabration.html">COLLABORATIONS</a></p>
-`
-}
-
-export default nav;
